d005946c2a40ca53b3ff349db28635cac459ceda
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-09-02 23:56:55 +0900

    python

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-python
+
+### python 은 print라고 쓴다. 신기함 ;도 안씀
--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print("hello world!")
+print("good")
+print(2+2) #4로 출력됨
+print('yes'*10) #yes 10번 실행해줌
+
+#변수
+name = '슈퍼 에이전트 초필살 드래곤'
+
+print(name)
+
+print(name[0]) #0부터 시작함 , 출력 결과:슈
+print(name[0:4]) #4번째 까지
