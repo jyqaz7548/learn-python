f7ae1aaa32dc66048eada2f6c8ed9fe625071d5a
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-20 19:26:29 +0900

    if문

--- jocoding/chapter3/3-1.py
+++ jocoding/chapter3/3-1.py
@@ -3,3 +3,38 @@ if money:
     print("부자")
 else :
     print("거지")
+
+
+x =3
+y= 2
+print(x>y)
+
+money = 2000
+card = True
+
+if money >= 3000 and card :
+    print("택시를 타고가라")
+else:
+    print("걸어가라")
+
+print(1 in [1,2,3])
+
+
+print("p " in 'python')
+
+
+pocket = ['paper','cellphone']
+card = True
+
+if 'money' in pocket :print("택시")
+
+elif 'paper' not in pocket :print("택시를 타고가라")
+
+else :
+    print("걸어가라")
+
+
+score = 100
+
+message = "good" if score >= 60 else "fail"
+print(message)
